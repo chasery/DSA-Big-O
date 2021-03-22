df89dd31d5e762bda946c54cd29ea5fd07ba750e
Ryan Chase <ryan@chasery.com>
2021-03-22 08:46:26 -0700

    Question eleven

--- rsa-big-o.js
+++ rsa-big-o.js
@@ -136,4 +136,45 @@
     }
 
     Answer: Constant time O(1); We return a randomly accessed array element.
+
+  10. What Am I?
+    What does the following algorithm do? What is the Big O of the following algorithm? Explain your answer
+
+    function isWhat(n) {
+        if (n < 2 || n % 1 !== 0) {
+            return false;
+        }
+        for (let i = 2; i < n; ++i) {
+            if (n % i === 0) return false;
+        }
+        return true;
+    }
+
+    Answer:
+
+  11. Tower of Hanoi
+    The Tower of Hanoi is a very famous mathematical puzzle (some call it game!). This is how it goes:
+
+    There are three rods and a number of disks of different sizes which can slide onto any rod. The puzzle starts with the disks neatly stacked in ascending order of size on one rod, the smallest disk at the top (making a conical shape). The other two rods are empty to begin with.
+    The goal of the puzzle is to move the entire stack of rods to another rod (can't be the original rod where it was stacked before) where it will be stacked in the ascending order as well. This should be done obeying the following rules: i) Only one disk may be moved at a time ii) Each move consists of taking the upper disk from one of the rods and sliding it onto another rod, on top of the other disks that may already be present on that rod. iii) A larger disk may not placed on top of a smaller disk
+
+    Derive an algorithm to solve the Tower of Hanoi puzzle.
+    Implement your algorithm using recursion. Your program should display each movement of the disk from one rod to another.
+    If you are given 5 disks, how do the rods look like after 7 recursive calls?
+    How many moves are needed to complete the puzzle with 3 disks? with 4 disks? with 5 disks?
+    What is the runtime of your algorithm?
 */
+
+const tower = (height, srcRod, desRod, tempRod) => {
+  if (height >= 1) {
+    // Move a tower of height-1 to the temp rod, using the destination rod.
+    tower(height - 1, srcRod, tempRod, desRod);
+    // Move the remaining disk to the destination peg.
+    console.log('Move disk from Tower', srcRod, 'to Tower', desRod);
+    // Move the tower of `height-1` from the `temp rod` to the `destination rod` using the `source rod`.
+    tower(height - 1, tempRod, desRod, srcRod);
+  }
+  return;
+};
+
+tower(4, '1', '2', '3');
